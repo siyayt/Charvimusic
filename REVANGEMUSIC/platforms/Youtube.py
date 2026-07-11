4ad63f685121dc04be56bcfd39ebb71045a3c5ca
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-07-11 22:53:41 +0530

    Modify API URL and key in Youtube.py
    Updated API URL and API key for Shruti API.

--- REVANGEMUSIC/platforms/Youtube.py
+++ REVANGEMUSIC/platforms/Youtube.py
@@ -5,12 +5,12 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch, Playlist
-import aiohttp
+from pyrogram.types import Message
+from youtubesearchpython.future import VideosSearch
 
-API_URL = os.environ.get("SHRUTI_API_URL", "https://api.shrutibots.site")
+API_URL = os.environ.get("SHRUTI_API_URL", "https://api01.shrutibots.site")
 
-API_KEY = os.environ.get("SHRUTI_API_KEY", "YOUR_API_KEY") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
+API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBots3OYSuzKa7u0PyQi3ifqT") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
 
 DOWNLOAD_DIR = "downloads"
 
